8e019a710d12e0d07e92f5e3acd8563e0c9c3ed5
Dutu1337 <46423190+dumitru1216@users.noreply.github.com>
2023-04-04 11:21:59 -0700

    create_device function

--- workspace-v2/entry_point/entry_point.cpp
+++ workspace-v2/entry_point/entry_point.cpp
@@ -10,7 +10,8 @@ void entry::impl::reset_device( ) {
 	HRESULT result{}; /* this is main result var, we use {} to initialize it */
 
 	/* we want our code look shiny so im gonna do that retarded shit
-		oxygen and whoever sees that code please forgive me */
+		oxygen and whoever sees that code please forgive me 
+	*/
 	auto reset_d3d_revice = [ ]( D3DPRESENT_PARAMETERS reset_parrameter ) -> HRESULT {
 		/* this shit might crash or might work, im not sure yet we will see once we run it :) */
 		g_device->Reset( &reset_parrameter );
@@ -43,4 +44,50 @@ void entry::impl::clean_device( ) {
 		/* after we released our d3d pointer we also want to set it to null/nullptr */
 		g_pd3d = NULL;
 	}
+}
+
+/* this function creates a directx_d3d device with a swapchain for rendering on a window, 
+	it sets up the presentation parameters for the device, if the device is created successfully, 
+	the function returns true, otherwise it returns false. 
+	The function uses global variables g_pd3d and g_device, which are assumed to be initialized elsewhere.
+*/
+vs_bool entry::impl::create_device( HWND hwnd ) {
+	/* lets create it */
+	if ( ( g_pd3d = direct_3d_create( D3D_SDK_VERSION ) ) == NULL )
+		return vs_false; /* classic, dinosaur epoca */
+
+	/* create the device */
+	/* initialize presentation parameters to zero */
+	zero_memory( &g_d3d9pp, sizeof( g_d3d9pp ) );
+
+	/* set windowed mode */
+	d3d_windowed = vs_true;
+
+	/* set swap effect */
+	d3d_swap_effect = d3d_swap_effect_discard;
+
+	/* set back buffer format */
+	d3d_back_buffer_format = d3d_fmt_unknown;
+
+	/* enable auto depth stencil */
+	d3d_enable_depth_stencil = vs_true;
+
+	/* set depth stencil buffer format */
+	d3d_auto_depth_stencil_format = d3d_fmt_16;
+
+	/* set presentation interval to one (present with vsync) */
+	d3d_presentation_interval = D3DPRESENT_INTERVAL_ONE;
+
+	/* we want our code look shiny so im gonna do that retarded shit
+		oxygen and whoever sees that code please forgive me, i fucking love snake_case 
+	*/
+	auto create_device = [ ]( HWND hwnd ) -> vs_bool {
+		/* ok i know i said i love snake_case but im not fucking that retarded so i redefine everything snake_case */
+		if ( g_pd3d->CreateDevice( D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hwnd, D3DCREATE_HARDWARE_VERTEXPROCESSING, &g_d3d9pp, &g_device ) < 0 )
+			return vs_false;
+	}; /* now we must create it (call it) */
+	create_device( hwnd ); /* this now will create our device */
+
+	/* now lets return true */
+	return vs_true;
 }
--- workspace-v2/entry_point/entry_point.hpp
+++ workspace-v2/entry_point/entry_point.hpp
@@ -11,20 +11,6 @@
 /* include all vs needs */
 #include "../includes.hpp"
 
-/* do some defines */
-/* you dont realise how much i hate those BOOL stuff in big case so we gonna make them small case */
-#define vs_bool BOOL
-
-/* entry_point namespace */
-namespace entry {
-	class impl {
-		void reset_device( );
-		void clean_device( );
-		vs_bool create_device( );
-	};
-	inline const auto g_entry = std::make_unique<impl>( );
-}
-
 /* those are main vars, i dont really want to bother to add them in the class
 	you can do that if you want tho
 */
@@ -32,3 +18,33 @@ static LPDIRECT3D9 g_pd3d = NULL;
 static LPDIRECT3DDEVICE9 g_device = NULL;
 static D3DPRESENT_PARAMETERS g_d3d9pp = {};
 static HWND g_window;
+
+/* do some defines */
+/* you dont realise how much i hate those BOOL stuff in big case so we gonna make them small case */
+/* fuck you visual studio */
+#define vs_bool BOOL
+#define vs_true TRUE
+#define vs_false FALSE
+#define direct_3d_create Direct3DCreate9
+#define zero_memory ZeroMemory
+
+/* d3d defines, i know this seems so fucking retarded but i love lowercase and snake_case */
+#define d3d_windowed g_d3d9pp.Windowed
+#define d3d_swap_effect g_d3d9pp.SwapEffect
+#define d3d_back_buffer_format g_d3d9pp.BackBufferFormat
+#define d3d_enable_depth_stencil g_d3d9pp.EnableAutoDepthStencil
+#define d3d_auto_depth_stencil_format g_d3d9pp.AutoDepthStencilFormat
+#define d3d_presentation_interval g_d3d9pp.PresentationInterval
+#define d3d_swap_effect_discard D3DSWAPEFFECT_DISCARD
+#define d3d_fmt_unknown D3DFMT_UNKNOWN
+#define d3d_fmt_16 D3DFMT_D16
+
+/* entry_point namespace */
+namespace entry {
+	class impl {
+		void reset_device( );
+		void clean_device( );
+		vs_bool create_device( HWND hwnd );
+	};
+	inline const auto g_entry = std::make_unique<impl>( );
+}
