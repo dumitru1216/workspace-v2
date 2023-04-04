474a7e10aa1b4dc5f48216c01734d9b173dc29c8
Dutu1337 <46423190+dumitru1216@users.noreply.github.com>
2023-04-04 10:48:54 -0700

    reset_device void + added handled main function

--- workspace-v2/entry_point/entry_point.cpp
+++ workspace-v2/entry_point/entry_point.cpp
@@ -1,5 +1,11 @@
+#include "entry_point.hpp"
 
-// this is here so we can compile for now
-void main( ) {
+int main( HINSTANCE h_instance, HINSTANCE h_prev_instance, 
+		  LPWSTR lp_cmd_line, int n_cmd_show ) {
+	/* main function so we can build and we also here call everything so yeah... */
+}
+
+/* reset device function, we use that to reset or result d3d window */
+void entry::impl::reset_device( ) {
 
 }
--- workspace-v2/entry_point/entry_point.hpp
+++ workspace-v2/entry_point/entry_point.hpp
@@ -14,7 +14,15 @@
 /* entry_point namespace */
 namespace entry {
 	class impl {
-
+		void reset_device( );
 	};
 	inline const auto g_entry = std::make_unique<impl>( );
 }
+
+/* those are main vars, i dont really want to bother to add them in the class
+	you can do that if you want tho
+*/
+static LPDIRECT3D9 g_pd3d = NULL;
+static LPDIRECT3DDEVICE9 g_device = NULL;
+static D3DPRESENT_PARAMETERS g_d3d9pp = {};
+static HWND g_window;
