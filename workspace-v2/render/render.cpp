b3c47c75c97df3347538a88dfd55b554ea033d61
Dutu1337 <46423190+dumitru1216@users.noreply.github.com>
2023-04-06 09:00:03 -0700

    rendering

--- workspace-v2/render/render.cpp
+++ workspace-v2/render/render.cpp
@@ -4,10 +4,18 @@ void render::impl::initialize_fonts( ) {
 	/* here we are going to initialize our fonts */
 }
 
-void render::impl::add_rect_filled( int x, int y, int w, int h, color color, int rounding ) {
-	draw_list->AddRectFilled( im_vec_2( x, y ), im_vec_2( x + w, y + h ), color.to_d3d( ), rounding );
+void render::impl::add_rect_filled( int x, int y, int w, int h, color c, int rounding ) {
+	draw_list->AddRectFilled( im_vec_2( x, y ), im_vec_2( x + w, y + h ), c.to_d3d( ), rounding );
 }
 
-void render::impl::add_rect( int x, int y, int w, int h, color color, int rounding ) {
-	draw_list->AddRect( im_vec_2( x, y ), im_vec_2( x + w, y + h ), color.to_d3d( ), rounding );
+void render::impl::add_rect( int x, int y, int w, int h, color c, int rounding ) {
+	draw_list->AddRect( im_vec_2( x, y ), im_vec_2( x + w, y + h ), c.to_d3d( ), rounding );
+}
+
+void render::impl::add_gradient_horizontal( int x, int y, int w, int h, color c1, color c2 ) {
+	draw_list->AddRectFilledMultiColor( im_vec_2( x, y ), im_vec_2( x + w, y + h ), c1.to_d3d( ), c2.to_d3d( ), c2.to_d3d( ), c1.to_d3d( ) );
+}
+
+void render::impl::add_gradient_horizontal( int x, int y, int w, int h, color c1, color c2 ) {
+	draw_list->AddRectFilledMultiColor( im_vec_2( x, y ), im_vec_2( x + w, y + h ), c1.to_d3d( ), c1.to_d3d( ), c2.to_d3d( ), c2.to_d3d( ) );
 }
--- workspace-v2/render/render.hpp
+++ workspace-v2/render/render.hpp
@@ -13,8 +13,11 @@ namespace render {
 		font_t fonts;
 
 		/* initialize part */
-		void add_rect_filled( int x, int y, int w, int h, color color, int rounding = 0.f );
-		void add_rect( int x, int y, int w, int h, color color, int rounding = 0.f );
+		void add_rect_filled( int x, int y, int w, int h, color c, int rounding = 0.f );
+		void add_rect( int x, int y, int w, int h, color c, int rounding = 0.f );
+		void add_gradient_vertical( int x, int y, int w, int h, color c, color c1 );
+		void add_gradient_horizontal( int x, int y, int w, int h, color c, color c1 );
+
 		void initialize_fonts( );
 	};
 	const auto g_render = std::make_unique<impl>( );
